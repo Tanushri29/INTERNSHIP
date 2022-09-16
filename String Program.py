500256a29db741aa23bdc5130f61db7e2a3935f6
Tanushri Solanki <106384207+Tanushri29@users.noreply.github.com>
2022-09-16 23:15:04 +0530

    Add files via upload

---
+++ String Program.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+str_1= input("Enter the string to check if it is a Palindrome:")
+str_1= str_1.casefold()
+rev_str = reversed(str_1)
+if list (str_1) == list (rev_str):
+     print("The string is Palindrome")
+else:
+     print("The string is not Palindrome")
+
+
+# In[ ]:
+
+
+
+
